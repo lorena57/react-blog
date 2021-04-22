dfd7861e67ba23ee87e7ba5fdfa247b1aaf4611e
Lorena C <lorena@eunknown.org>
2021-04-22 15:06:27 -0700

    add components

---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './style.css';
+
+function Hero(props) {
+  return (
+    <div>
+      <Card>Lorena is the best</Card>
+    </div>
+  );
+}
+
+export default Hero;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Header from '../../components/Header';
 import './style.css';
 
 const Home = (props) => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
